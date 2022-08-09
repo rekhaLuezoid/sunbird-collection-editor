2132ec1fcb896f15f9df10a43a037ee89c9a386c
rekha <rekha.sharma@luezoid.com>
2022-08-09 18:59:47 +0530

    Quality Params- WIP

---
+++ projects/collection-editor-library/src/lib/components/quality-params-modal/quality-params-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-quality-params-modal',
+  templateUrl: './quality-params-modal.component.html',
+  styleUrls: ['./quality-params-modal.component.css']
+})
+export class QualityParamsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
